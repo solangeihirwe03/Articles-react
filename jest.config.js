0bbb328fbe185503aa9800a44dd777997b8e94d6
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2025-01-03 16:07:02 +0200

    new changes

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-
-export default {
-    testEnvironment: "jsdom",
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$' : 'babel-jest'
-    },
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
